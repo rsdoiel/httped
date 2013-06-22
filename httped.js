96207ada213e0d494e1a4b333a7a54427e649a36
R. S. Doiel <rsdoiel@usc.edu>
2013-06-21 19:18:43 -0700

    Stubbing out POST support.

--- httped.js
+++ httped.js
@@ -1,7 +1,8 @@
-/**
+/*!
  * httped.js - A web browser friendly wrapper of xhr.
  * @author R. S. Doiel, <rsdoiel@gmail.com>
- * copyright (c) R. S. Doiel
+ *
+ * copyright (c) 2013 R. S. Doiel
  * all rights reserved.
  * Released under the BSD 2-clause license.
  * See http://opensource.org/licenses/BSD-2-clause for details.  
@@ -11,6 +12,23 @@
   "use strict";
   var HTTPED = new Object();
    
+  /*
+   * Perform an http GET operation and return the results in
+   * either an "onComplete" or "onStateChange" callback.
+   * @method Get
+   * @param {string} a URL to fetch
+   * @param {function} an "onComplete" callback. Callback will
+   * receive a http status code as the first parameter followed by the
+   * response received from the xhr request.
+   * @param {function} an "onStateChange" callback. Callback will
+   * receive a "state" code and the response object from the xhr request.
+   * States can be
+   * 0. unsent
+   * 1. opened
+   * 2. headers received
+   * 3. loading
+   * State 4 is given to the "onComplete" callback instead of "onStateChange".
+   */
   function Get(url, onComplete, onStateChange) {
     var xhr;
 
@@ -31,7 +49,7 @@
       throw("remote xhr not supported.");
     }    
     
-    // Attach listeners for compelte and on state change
+    // Manage the state changes and trigger appropriate callback.
     function manageStateChange () {
       if (xhr.readyState === 4) {
         // Two params, http status, response
@@ -45,7 +63,18 @@
     xhr.open('GET', url);
     xhr.send(null);
   };
-
   HTTPED.Get = Get;
+
+  /*
+   * Post will prep and send a post to the URL provided.
+   * @method Post
+   * @param {string} the URL to post to.
+   */
+  function Post(url, data) {
+    //FIXME: implement data encoding and transmition.
+    thow("Post not implemented.");
+  }
+  HTTPED.Post = Post;
+
   global.HTTPED = HTTPED;
 }(this));
