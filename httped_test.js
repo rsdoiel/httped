c65b24095b3e3c509db2d618e52da5de56bdb26e
R. S. Doiel <rsdoiel@gmail.com>
2013-06-21 19:22:32 -0600

    Update httped_test.js

--- httped_test.js
+++ httped_test.js
@@ -1,3 +1,29 @@
 /**
  * httped_test.js - Test code to make sure httped_test works in the browser.
  */
+YUI().use("test", function (Y) {
+  var testCase,
+    bbc_tech_feed = "http://feeds.bbci.co.uk/news/technology/rss.xml",
+    example_com_feed = "http://doesnotexists.example.com/feed/rss.xml";
+  
+  testCase = new Y.Test.TestCase({
+    name: "Get http GET to see if HTTPED.GET() works",
+    "Should fetch fata if URL is a valid source": function () {
+        Get(bbc_tech_feed, function(httpStatus, response) {
+            Y.Assert.areEqual(httpStatus, 200);
+        }, function (readyState, response) {
+            Y.Assert.areNotEqual(readyState, 4);
+        });
+    },
+    "Should not fetch data is URL isn't valid": function () {
+        Get(bbc_tech_feed, function(httpStatus, response) {
+            Y.Assert.areNotEqual(httpStatus, 200);
+        }, function (readyState, response) {
+            Y.Assert.areNotEqual(readyState, 4);
+        });
+    }
+  });
+  Y.Test.Runner.add(testCase);
+  
+  Y.Test.Runner.run();
+});
