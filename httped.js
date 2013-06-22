5cbd7919d929e90842a95bebe5fdfce692d182f9
R. S. Doiel <rsdoiel@gmail.com>
2013-06-21 19:03:51 -0600

    Update httped.js

--- httped.js
+++ httped.js
@@ -5,4 +5,44 @@
 /*jslint browser: true, indent: 4 */
 (function (global) {
    "use strict";
+   var HTTPED = new Object();
+   
+   function Get(url, onComplete, onStateChange) {
+      var httpRequest;
+      // do stuff
+      console.log("url", url);// DEBUG
+      
+      // Taken from Mozilla's Ajax example
+      if (window.XMLHttpRequest) { // Mozilla, Safari, ...
+          httpRequest = new XMLHttpRequest();
+      } else if (window.ActiveXObject) { // IE 8 and older
+         try {
+           httpRequest = new ActiveXObject("Msxml2.XMLHTTP");
+         } 
+         catch (e) {
+           try {
+             httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+           } 
+           catch (e) {}
+         }
+      }
+      if (!httpRequest) {
+         throw("remote http requests not supported.");
+      }      
+      
+      // Attach listeners for compelte and on state change
+      function manageStateChange () {
+         if (httpRequest.readyState === 4) {
+            // Two params, http status, response
+            onComplete(httpRequest.status, httpRequest);
+         } else {
+            onStateChange(httpRequest.readyState, httpRequest);
+         }
+      }
+      
+      httpRequest.onreadystatechange = manageStateChange;
+   };
+   HTTPED.Get = Get;
+
+   global.HTTPED;
 }(this));
