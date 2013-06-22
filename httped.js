f4f51eb589500c796af0f8d788b9531c82ff9954
R. S. Doiel <rsdoiel@gmail.com>
2013-06-21 18:36:56 -0600

    Create httped.js

---
+++ httped.js
@@ -0,0 +1,8 @@
+/**
+ * httped.js - A web browser friendly wrapper of xhr.
+ * @author R. S. Doiel, <rsdoiel@gmail.com>
+ */
+/*jslint browser: true, indent: 4 */
+(function (global) {
+   "use strict";
+}(this));
