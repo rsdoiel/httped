d1f0536b3c6c8310c09c776b4cc27200aa462de2
R. S. Doiel <rsdoiel@usc.edu>
2013-06-21 19:00:22 -0700

    snapshot

--- httped.js
+++ httped.js
@@ -2,46 +2,46 @@
  * httped.js - A web browser friendly wrapper of xhr.
  * @author R. S. Doiel, <rsdoiel@gmail.com>
  */
-/*jslint browser: true, indent: 4 */
+/*jslint browser: true, indent: 2 */
 (function (global) {
-   "use strict";
-   var HTTPED = new Object();
+  "use strict";
+  var HTTPED = new Object();
    
-   function Get(url, onComplete, onStateChange) {
-      var httpRequest;
+  function Get(url, onComplete, onStateChange) {
+    var xhr;
 
-      // This is derived on the example at Mozilla's dev site
-      // https://developer.mozilla.org/en-US/docs/AJAX/Getting_Started
-      if (window.XMLHttpRequest) { // Mozilla, Safari, ...
-          httpRequest = new XMLHttpRequest();
-      } else if (window.ActiveXObject) { // IE 8 and older
-         try {
-           httpRequest = new ActiveXObject("Msxml2.XMLHTTP");
-         } catch (e) {
-           try {
-             httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
-           } catch (e) {}
-         }
+    // xhr handling is derived from code samples at Mozilla's dev site
+    // https://developer.mozilla.org/en-US/docs/AJAX/Getting_Started
+    if (window.XMLHttpRequest) { // Mozilla, Safari, ...
+      xhr = new XMLHttpRequest();
+    } else if (window.ActiveXObject) { // IE 8 and older
+      try {
+        xhr = new ActiveXObject("Msxml2.XMLHTTP");
+      } catch (e) {
+        try {
+          xhr = new ActiveXObject("Microsoft.XMLHTTP");
+        } catch (e) {}
       }
-      if (!httpRequest) {
-         throw("remote xhr not supported.");
-      }      
-      
-      // Attach listeners for compelte and on state change
-      function manageStateChange () {
-         if (httpRequest.readyState === 4) {
-            // Two params, http status, response
-            onComplete(httpRequest.status, httpRequest.response);
-         } else {
-            onStateChange(httpRequest.readyState, httpRequest.response);
-         }
+    }
+    if (!xhr) {
+      throw("remote xhr not supported.");
+    }    
+    
+    // Attach listeners for compelte and on state change
+    function manageStateChange () {
+      if (xhr.readyState === 4) {
+        // Two params, http status, response
+        onComplete(xhr.status, xhr.response);
+      } else {
+        onStateChange(xhr.readyState, xhr.response);
       }
+    }
 
-      httpRequest.onreadystatechange = manageStateChange;
-      httpRequest.open('GET', url);
-      httpRequest.send(null);
-   };
-   HTTPED.Get = Get;
+    xhr.onreadystatechange = manageStateChange;
+    xhr.open('GET', url);
+    xhr.send(null);
+  };
 
-   global.HTTPED;
+  HTTPED.Get = Get;
+  global.HTTPED = HTTPED;
 }(this));
--- httped_test.js
+++ httped_test.js
@@ -1,29 +1,34 @@
 /**
  * httped_test.js - Test code to make sure httped_test works in the browser.
  */
+/*jslint browser: true, indent: 2 */
 YUI().use("test", function (Y) {
-  var testCase,
-    bbc_tech_feed = "http://feeds.bbci.co.uk/news/technology/rss.xml",
-    example_com_feed = "http://doesnotexists.example.com/feed/rss.xml";
+  var Get = HTTPED.Get,
+    testCase,
+    good_feed = "http://web-app.usc.edu/ws/url-cache/api/ecal3/highlights/32";
+    bad_feed = "http://doesnotexists.example.com/feed/rss.xml";
   
   testCase = new Y.Test.TestCase({
     name: "Get http GET to see if HTTPED.GET() works",
+    "should have a well formed the HTTPED object": function () {
+        Y.Assert.isObject(HTTPED);
+        Y.Assert.isTrue(typeof HTTPED.Get === "function");
+    },
     "Should fetch fata if URL is a valid source": function () {
-        Get(bbc_tech_feed, function(httpStatus, response) {
-            Y.Assert.areEqual(httpStatus, 200);
-        }, function (readyState, response) {
-            Y.Assert.areNotEqual(readyState, 4);
-        });
+      Get(good_feed, function(httpStatus, response) {
+        Y.Assert.areEqual(200, httpStatus);
+      }, function (readyState, response) {
+        Y.Assert.areNotEqual(4, readyState);
+      });
     },
     "Should not fetch data is URL isn't valid": function () {
-        Get(bbc_tech_feed, function(httpStatus, response) {
-            Y.Assert.areNotEqual(httpStatus, 200);
-        }, function (readyState, response) {
-            Y.Assert.areNotEqual(readyState, 4);
-        });
+      Get(bad_feed, function(httpStatus, response) {
+        Y.Assert.areNotEqual(200, httpStatus);
+      }, function (readyState, response) {
+        Y.Assert.areNotEqual(4, readyState);
+      });
     }
   });
   Y.Test.Runner.add(testCase);
-  
   Y.Test.Runner.run();
 });
